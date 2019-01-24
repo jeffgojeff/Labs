52488adafa096843738c954455b5b3748503a62d
jeffgojeff <jeffgojeff@gmail.com>
2019-01-23 18:39:37 -0600

    Add files via upload

---
+++ preLab7.cpp
@@ -0,0 +1,36 @@
+// Lab 7 Pre-Lab Assignment
+
+// Program takes 5 numbers from a user (from console), stores them into an
+// array, and then prints them to the screen (on the same line).
+
+// Add code to complete this program. You only need to add code where indicated
+// by "ADD HERE".
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int SIZE = 5;  // size of array
+
+	// ADD HERE - create an array of integers that will hold 5 integers.
+	int array[5];
+
+	cout << "please enter 5 integers : " << endl;
+	// For loop takes 5 integers from user and stores them in the array
+	for (int i = 0 ; i < SIZE ; i++)
+	{
+		// ADD HERE - take user's input and store it in the array you created
+		cin >> array[i];
+    	
+	}
+
+	// ADD HERE - Write a for loop to print the array to the screen
+	cout << endl;	//added for easy interpretation in terminal
+	for (int i = 0; i < SIZE; ++i)
+	{
+		cout << array[i] << ",";
+	}
+	cout << endl;	//added for easy interpretation in terminal
+	return 0;
+}
---
+++ preLab9.cpp
@@ -0,0 +1,77 @@
+// Lab 9 Pre-Lab Assignment
+//Jeffrey C. Slocum
+
+// Add the following functions:
+
+// 1. 'getRadius' asks the user to enter the radius of a circle
+//     and returns the given value. (should return a double)
+
+// 2. 'calcArea' takes the radius and returns the area of a circle.
+
+// 3. 'printResults' void type function that should print the results to 
+//    console. 
+
+// Your function needs to have a local variable called 'PI' which holds the
+// value '3.14159'.
+
+// The function call is provided, you just need to implement the
+// function prototype and function definition.
+
+#include <iostream>
+using namespace std;
+
+// TODO - write the prototypes here
+double getRadius();
+double calcArea(double);
+void printResults();
+
+int main()
+{
+    double radius,
+           area;
+
+    // TODO - call 'getRadius' function and assign the 
+    //        returned value to radius.
+    radius = getRadius();
+
+
+    area = calcArea(radius);
+
+    // TODO - move the following cout to the 'printResults' function 
+    //        and call that function here instead.
+    printResults();
+
+    return 0;
+
+}
+
+// TODO - write the functions here
+
+double getRadius()
+{
+    double radius;
+    cout << "Enter the radius of a circle: " << endl;
+    cin >> radius;
+    return radius;
+}
+
+double calcArea(double A)
+{
+   double PI = 3.14159;
+   double total = PI * A * A;
+
+   return total;
+
+}
+
+void printResults()
+{
+    double area;
+    cout << "The area of your circle is " << area << " units squared." 
+         << endl;
+}
+
+
+
+
+
